dc4d192c173e8b52b7d53527d975be322d62b224
Sajal Sisodia <sajal89304@gmail.com>
2025-06-24 15:09:52 +0530

    basic setup

---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./routes/App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Bag from "./routes/Bag.jsx";
+import Home from './routes/Home.jsx'
+import Sajal from "./routes/Sajal.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/bag",
+        element: <Bag />,
+      },
+      {
+        path: "/sajal",
+        element: <Sajal />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
+
+
---
+++ src/routes/Sajal.jsx
@@ -0,0 +1,10 @@
+
+
+
+const Sajal = () => {
+  return(
+    <h1>THsis is  page </h1>
+  )
+}
+
+export default Sajal;
